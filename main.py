2461b411900582d6e8ec99b342fe12f5f95a4b79
alexandre <alexandremr01@gmail.com>
2022-11-20 10:53:00 +0100

    standardize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import  streamlit_toggle as tog
 
 import numpy as np
 import  pandas as pd
@@ -56,6 +55,13 @@ measurements = pd.read_csv('data/mesures_exposition_sondes_autonomes.csv', delim
 measurements['date']= pd.to_datetime(measurements['date'])
 measurements['day'] = measurements['date'].map(lambda x: x.date())
 
+means = measurements[['numero', 'E_volt_par_metre']].groupby('numero').mean().rename(columns={'E_volt_par_metre': 'mean'})
+stds = measurements[['numero', 'E_volt_par_metre']].groupby('numero').std().rename(columns={'E_volt_par_metre': 'std'})
+measurements = pd.merge(measurements, means, on='numero')
+measurements = pd.merge(measurements, stds, on='numero')
+measurements['E_volt_par_metre'] = (measurements['E_volt_par_metre'] - measurements['mean'] ) / measurements['std']
+
+
 mean = measurements[['day', 'E_volt_par_metre'] ].groupby('day').mean().reset_index()
 mean['day'] = pd.to_datetime(mean['day']).map(lambda x: x.date()).astype('datetime64[ns]')
 
@@ -68,7 +74,7 @@ def get_highest_frequencies(ffty_half, n_max=5):
     values = np.round(periods[np.sort(idx)]) / 24
     return max[:n_max], values[:n_max]
 
-def plot_sensor(sensor_name, start_date, end_date):
+def get_city_data(sensor_name, start_date, end_date):
     df_city = measurements[measurements['numero'] == sensor_name]
     df_city = df_city.sort_values(by='date')
     df_city['day'] = pd.to_datetime(df_city['date']).dt.date
@@ -79,59 +85,69 @@ def plot_sensor(sensor_name, start_date, end_date):
     df_new = pd.merge(df_new, df_city, on='day', how='left')
 
     df_new = pd.merge(df_new, mean, on='day', how='left')
-    fig = go.Figure()
+    return df_new
 
+def plot_sensor(df, sensor_name, lp_filtered):
+    fig = go.Figure()
     fig.update_layout(
         title="Exposure by day",
         xaxis_title="Date",
-        yaxis_title="Exposure - V/m",
+        yaxis_title="Standardized Exposure - V/m",
     )
-    fig.add_trace(go.Scatter(x=df_new['day'], y=df_new['E_volt_par_metre_x'],
+    fig.add_trace(go.Scatter(x=df['date'], y=df['E_volt_par_metre_x'],
                     mode='lines',
                     name=sensor_name))
-    fig.add_trace(go.Scatter(x=df_new['day'], y=df_new['E_volt_par_metre_y'],
+    fig.add_trace(go.Scatter(x=df['date'], y=df['E_volt_par_metre_y'],
                     mode='lines',
                     name='Sensors average'))
-                    
-    series = df_new['E_volt_par_metre_x'].to_numpy()
-    series = series[~np.isnan(series)]
+    fig.add_trace(go.Scatter(x=df['date'], y=lp_filtered,
+                    mode='lines',
+                    name='Lowpass'))
+    return fig
 
-    # result=seasonal_decompose(df['#Passengers'], model='multiplicable', period=12)
-    fft = abs(np.fft.fft(series))
-    fft_half = fft[:int(len(fft)/2)]
-
-    max_frequencies, periods = get_highest_frequencies(fft_half)
-    pairs = zip(periods, max_frequencies)
-    
-    fft_x = np.array(list(range(len(fft_half[1:])))) * (1/2) / len(series)
-    fig2 = go.Figure(go.Scatter(x=fft_x, y=fft[1:]))
-    fig2.update_layout(
-        title="Fast Fourrier Transform (Omitting point at f=0)",
+def plot_fourrier(fftx, ffty, hide_first=False):
+    first_element = 1 if hide_first else 0 
+    fig = go.Figure(go.Scatter(x=fftx[first_element:], y=ffty[first_element:]))
+    fig.update_layout(
+        title="Fast Fourrier Transform",
         xaxis_title="Frequency (1/h)",
         yaxis_title="Amplitude",
     )
+    return fig
 
-    fft[50:] = 0
-    lp_filtered = np.real(np.fft.ifft(fft))
-    fig.add_trace(go.Scatter(x=df_new['day'], y=lp_filtered,
-                    mode='lines',
-                    name='Lowpass'))
+def plot_seasonal(result):  
+    fig3 = make_subplots(
+        rows=4, cols=1,
+        subplot_titles=["Observed", "Trend", "Seasonal", "Residuals"])
+    fig3.add_trace(
+        go.Scatter(x=result.seasonal.index, y=result.observed, mode='lines'),
+            row=1, col=1,
+        )
 
-    return fig, fig2, pairs
+    fig3.add_trace(
+        go.Scatter(x=result.trend.index, y=result.trend, mode='lines'),
+            row=2, col=1,
+        )
+
+    fig3.add_trace(
+        go.Scatter(x=result.seasonal.index, y=result.seasonal, mode='lines'),
+            row=3, col=1,
+        )
+
+    fig3.add_trace(
+        go.Scatter(x=result.resid.index, y=result.resid, mode='lines'),
+            row=4, col=1,
+        )
+    return fig3
 
 
 st.set_page_config(layout="wide")
 st.title("Antennas and sensors")
 st.write("Use the following map to search for antennas and sensors")
-reference_date = st.date_input("Reference date")
 
-val = tog.st_toggle_switch(label="Full map", 
-                default_value=False, 
-                label_after = False, 
-                inactive_color = '#D3D3D3', 
-                active_color="#11567f", 
-                track_color="#29B5E8"
-                )
+
+reference_date = st.date_input("Reference date")
+val = st.checkbox("Full map")
 fig = get_map(antenna, sensors, reference_date,open_view=val)
 st.plotly_chart(fig, use_container_width=True,)
 
@@ -143,9 +159,40 @@ with c2:
 with c3:
     end_date = st.date_input("End date")
 
-fig_sensor, fig_fourrier, pairs = plot_sensor(selected_sensor, start_date, end_date)
-st.plotly_chart(fig_sensor, use_container_width=True,)
-st.plotly_chart(fig_fourrier, use_container_width=True,)
+df_city = get_city_data(selected_sensor, start_date, end_date)
+series = df_city['E_volt_par_metre_x']
+series = pd.Series(np.where(np.isnan(series), series.interpolate(method='linear'), series))
+series = series[~np.isnan(series)]
 
-for pair in pairs:
-    st.write(f'Period {pair[0]}h, amplitude {pair[1]}')
+ffty = abs(np.fft.fft(series))
+fft_half = ffty[:int(len(ffty)/2)]
+max_frequencies, periods = get_highest_frequencies(fft_half)
+pairs = zip(periods, max_frequencies)
+fftx = np.array(list(range(len(fft_half[1:])))) * (1/2) / len(series)
+
+def lowpass(ffty):
+    ffty_aux = ffty.copy()
+    ffty_aux[50:] = 0
+    return np.real(np.fft.ifft(ffty_aux))
+
+lp_filtered = lowpass(ffty)
+
+fig_sensor = plot_sensor(df_city, selected_sensor, lp_filtered)
+# result = seasonal_decompose(series, model='multiplicable', period=7*24*2)
+# fig_seasonal = plot_seasonal(result)
+
+st.plotly_chart(fig_sensor, use_container_width=True,)
+
+tab1, tab2 = st.tabs(["Fourrier", "Seasonal"])
+
+with tab1:
+    hide_f0 = st.checkbox("Hide f=0")
+    fig_fourrier = plot_fourrier(fftx, ffty, hide_f0)
+
+    st.plotly_chart(fig_fourrier, use_container_width=True,)
+    for pair in pairs:
+        st.write(f'Period {pair[0]} days, amplitude {pair[1]}')
+
+# with tab2:
+
+#     st.plotly_chart(fig_seasonal, use_container_width=True,)
