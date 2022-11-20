e3a7d1c8e6179c2383ee267d4645886d981ec4fd
alexandre <alexandremr01@gmail.com>
2022-11-20 03:13:41 +0100

    fix main

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ plotly.offline.init_notebook_mode(connected = True)
 import plotly.io as pio
 import plotly.express as px
 
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 pio.renderers.default = 'colab'
 
@@ -58,6 +59,14 @@ measurements['day'] = measurements['date'].map(lambda x: x.date())
 mean = measurements[['day', 'E_volt_par_metre'] ].groupby('day').mean().reset_index()
 mean['day'] = pd.to_datetime(mean['day']).map(lambda x: x.date()).astype('datetime64[ns]')
 
+def get_highest_frequencies(ffty_half, n_max=5):
+    max = np.argpartition(abs(ffty_half), -25)[-25:]
+    max = np.flip(max)
+    max = max[np.where((max > 3) ) ]
+    periods = 1 / (max / 2 / (2*len(ffty_half)))
+    _,idx = np.unique(np.round(periods), return_index=True)
+    values = np.round(periods[np.sort(idx)]) / 24
+    return max[:n_max], values[:n_max]
 
 def plot_sensor(sensor_name, start_date, end_date):
     df_city = measurements[measurements['numero'] == sensor_name]
@@ -83,7 +92,33 @@ def plot_sensor(sensor_name, start_date, end_date):
     fig.add_trace(go.Scatter(x=df_new['day'], y=df_new['E_volt_par_metre_y'],
                     mode='lines',
                     name='Sensors average'))
-    st.plotly_chart(fig, use_container_width=True,)
+                    
+    series = df_new['E_volt_par_metre_x'].to_numpy()
+    series = series[~np.isnan(series)]
+
+    # result=seasonal_decompose(df['#Passengers'], model='multiplicable', period=12)
+    fft = abs(np.fft.fft(series))
+    fft_half = fft[:int(len(fft)/2)]
+
+    max_frequencies, periods = get_highest_frequencies(fft_half)
+    pairs = zip(periods, max_frequencies)
+    
+    fft_x = np.array(list(range(len(fft_half[1:])))) * (1/2) / len(series)
+    fig2 = go.Figure(go.Scatter(x=fft_x, y=fft[1:]))
+    fig2.update_layout(
+        title="Fast Fourrier Transform (Omitting point at f=0)",
+        xaxis_title="Frequency (1/h)",
+        yaxis_title="Amplitude",
+    )
+
+    fft[50:] = 0
+    lp_filtered = np.real(np.fft.ifft(fft))
+    fig.add_trace(go.Scatter(x=df_new['day'], y=lp_filtered,
+                    mode='lines',
+                    name='Lowpass'))
+
+    return fig, fig2, pairs
+
 
 st.set_page_config(layout="wide")
 st.title("Antennas and sensors")
@@ -108,5 +143,9 @@ with c2:
 with c3:
     end_date = st.date_input("End date")
 
-plot_sensor(selected_sensor, start_date, end_date)
+fig_sensor, fig_fourrier, pairs = plot_sensor(selected_sensor, start_date, end_date)
+st.plotly_chart(fig_sensor, use_container_width=True,)
+st.plotly_chart(fig_fourrier, use_container_width=True,)
 
+for pair in pairs:
+    st.write(f'Period {pair[0]}h, amplitude {pair[1]}')
